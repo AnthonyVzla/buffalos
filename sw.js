fec531a3131dee8412bc31af19187f6416d34edf
AnthonyVzla <128426551+AnthonyVzla@users.noreply.github.com>
2026-08-19 15:40:14 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,25 @@
-const CACHE='buffalos-v1';
+const CACHE='buffalos-v2';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest'];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL))));
-self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{let copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match('./index.html'))))});
+self.addEventListener('install', event => {
+	self.skipWaiting();
+	event.waitUntil(caches.open('buffalos-v2').then(cache => cache.addAll(APP_SHELL)));
+});
+self.addEventListener('activate', event => {
+	event.waitUntil(
+		caches.keys()
+			.then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+			.then(() => self.clients.claim())
+	);
+});
+self.addEventListener('fetch', event => {
+	if (event.request.method !== 'GET') return;
+	event.respondWith(
+		fetch(event.request, { cache: 'no-store' })
+			.then(response => {
+				const copy = response.clone();
+				caches.open(CACHE).then(cache => cache.put(event.request, copy));
+				return response;
+			})
+			.catch(() => caches.match(event.request).then(cached => cached || caches.match('./index.html')))
+	);
+});
